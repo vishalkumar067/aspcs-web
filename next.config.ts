aad603da1a82f0b8c9e1baa7e8ce277bd6d62f92
Vishal Kumar <vishalkumar067@gmail.com>
2026-05-21 11:39:32 +0530

    Fix build errors

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,8 @@
 import type { NextConfig } from "next";
 
+import type { NextConfig } from "next";
+export default nextConfig;
+
 const nextConfig: NextConfig = {
   eslint: {
     ignoreDuringBuilds: true,
@@ -7,6 +10,9 @@ const nextConfig: NextConfig = {
   typescript: {
     ignoreBuildErrors: true,
   },
+
+
+
   images: {
     remotePatterns: [
       {
