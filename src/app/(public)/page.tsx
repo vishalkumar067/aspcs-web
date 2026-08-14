fd7abdfb663d9c534d8c38f88a25fdab1a772927
vishalkumar067 <vshlkmr256@gmail.com>
2026-08-14 10:38:26 +0530

    Add VideosSection to the public page

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -24,6 +24,7 @@ export default function HomePage() {
       <AboutSection />
       <ProgramsSection />
       <NoticesSection />
+      <VideosSection />
       <GallerySection />
       <CareersSection />
       <TCVerificationSection />
